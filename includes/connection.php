5317cd3dc50eec088f18ee4a27ea941d04dfa326
Christine <ycyrffgroupie@gmail.com>
2013-06-15 11:31:26 +0100

    Get it to work with the subdomain

--- includes/connection.php
+++ includes/connection.php
@@ -1,8 +1,8 @@
 <?
-$connection = new mysqli("localhost","ycyrf718_ycyrff","y1986Cyrff", "ycyrf718_cyrff") or die(mysql_error());
+$connection = mysql_connect("localhost","ycyrf718_ycyrff","y1986Cyrff") or die(mysql_error());
 //ycyrf718_holdingGuestbook
-if ($connection-> connect_errno){}
-//mysql_select_db($connection) or die(mysql_error());
+
+mysql_select_db("ycyrf718_cyrff", $connection) or die(mysql_error());
 
 //mysql_close($connection);
 
