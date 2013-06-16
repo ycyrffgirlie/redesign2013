2b439d050ceac70352ab3456ffe0e1dccc90b7c7
Christine <ycyrffgroupie@gmail.com>
2013-06-16 22:44:09 +0100

    added more code

--- profiles/fanprofile.php
+++ profiles/fanprofile.php
@@ -1,4 +1,34 @@
-<?php include '../includes/header.php'; ?>
+<?php 
+if (file_exists('../includes/connection.php')){
+	require('../includes/connection.php');
+}else{
+	require("connection.php");
+}
+
+
+if (isset( $_GET['profileid']) && ($_GET['profileid'] != NULL OR $_GET['profileid'] !='' )){
+	$profile_id = $_GET['profileid']; 
+	$sql = 'SELECT title FROM pages, fan_profile WHERE pages.filename = fan_profile.filename
+	AND profileid = '.$profile_id;
+
+	$query = mysql_query($sql) or die('The error is here');
+	if ($query ){
+		if ($titlequery = mysql_fetch_assoc($query)){
+		//;
+			$title = $titlequery["title"];
+		}else{
+			$title =  'Error:';
+		}
+	}else{
+		$title =  'Error:';
+	}
+}else{
+	$title =  'Error:';
+	$profile_id  = 500000;
+	//echo 'The else statement';
+}
+
+include '../includes/header.php'; ?>
 
 <body>
 <div class="container">
@@ -8,7 +38,9 @@
 	</ul>
 </div>
 
-<?=getprofile($profile_id)?>
+<h1><?=$title;?></h1>
+
+<?=getprofile($profile_id);?>
 
 
 
