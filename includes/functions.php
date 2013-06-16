cc90ec284bf5623ee8d2d4285829ad849b3f7d3e
Christine <ycyrffgroupie@gmail.com>
2013-06-16 22:41:15 +0100

    added the getfanprofile function

--- includes/functions.php
+++ includes/functions.php
@@ -5,9 +5,14 @@ function backButton($filename){
 	$link = substr_replace($filename, ' ', strrpos($filename, "/"));
 	//$output = '<p>'.$link.'</p>';
 	$link  = trim($link );
-	$internalURL = array ('http://'.$_SERVER[HTTP_HOST ].$link , 'http://'.$_SERVER[HTTP_HOST ].$link.'/' , 'http://'.$_SERVER[HTTP_HOST ].$link .'/index.html');
-	//$output = '<p class="internalURL">'.print_r($internalURL).'</p>';
 	
+	if ($filename == "/profiles/fanprofile.php"){
+		$internalURL = array ('http://'.$_SERVER[HTTP_HOST ].$link.'/profile.html');
+		$link  = 'http://'.$_SERVER[HTTP_HOST ].$link.'/profile.html';
+	}else{
+		$internalURL = array ('http://'.$_SERVER[HTTP_HOST ].$link , 'http://'.$_SERVER[HTTP_HOST ].$link.'/' , 'http://'.$_SERVER[HTTP_HOST ].$link .'/index.html');
+		//$output = '<p class="internalURL">'.print_r($internalURL).'</p>';
+	}
 	if (in_array($_SERVER[HTTP_REFERER],$internalURL)){
 		$output = '<script>
 $(document).ready(function(){
@@ -32,6 +37,45 @@ $(document).ready(function(){
 </div></a>';
 	}
 	
+	return $output;
+}
+
+function getprofile($profileid){
+	if (file_exists('/includes/connection.php')){
+	require('includes/connection.php');
+}else{
+	require("connection.php");
+}
+
+	$sql = 'SELECT 
+			profileid, name, place, website, email, firstheard,favesong, favealbum, comment
+			FROM fan_profile 
+			WHERE profileid ='.$profileid;
+	$query = mysql_query($sql);
+	$profileinfo = mysql_fetch_assoc($query);
+	
+	if ($profileinfo){
+	$output = '<p><strong>Name/Enw:</strong>&nbsp;'.$profileinfo['name'].'
+			<br>
+			<strong>Location/<span lang="cy">LLe:</span></strong>&nbsp;'.$profileinfo['place'].'
+			<br>
+			<strong>Website/<span lang="cy">Gwe safle:</span></strong>&nbsp;'.$profileinfo['website'].'
+			<br>
+			<strong>E-mail/<span lang="cy">E-bost:</span></strong>&nbsp;'.$profileinfo['email'].'
+			<br>
+			<strong>Where first heard about Cyrff?/<span lang="cy">LLe naethoch chi cyntaf wedi clywed am Y Cyrff?</span></strong>&nbsp;'.$profileinfo['firstheard'].'
+			<br>
+			<strong>Favourite Cyrff song/<span lang="cy">Hoff g&acirc;n Cyrff:</span></strong>&nbsp;'.$profileinfo['favesong'].'
+			<br>
+			<strong>Favorite Cyrff album/<span lang="cy">Hoff albwn Cyrff:</span></strong>&nbsp;'.$profileinfo['favealbum'].'
+			<br>
+			<strong>Other comments/<span lang="cy">Eraill sywl:</span></strong>&nbsp;'.$profileinfo['comment'].'
+			</p>';
+		}else{
+			$output = '<p>This profile doesn\'t exist</p>';
+		}
+	
+	mysql_close($connection);
 	return $output;
 }
 
