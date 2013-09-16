87689a1af65dad9c3910ffa03a97e33a191c49e1
Christine <ycyrffgroupie@gmail.com>
2013-09-16 11:02:11 +0100

    added languages files

---
+++ includes/lang/cy.about.php
@@ -0,0 +1,74 @@
+<?php
+
+$page1_link ="am.html";
+$page2_link ="am1.html";
+
+define("ENGLISH","Saesneg");
+define("WELSH","Cymraeg");
+define("PAGE_ONE","Tudalen 1");
+define("PAGE_TWO","Tudalen 2");
+
+
+define("PAGE_ONE_TEXT", "
+<style>
+.quotes{
+	font-style: italic;
+	margin: 0 auto;
+	width: 75%;
+}
+</style>
+<h1>Y Cyrff</h1>
+
+<p>Roedd Y Cyrff yn ffurfio yn Ysgol Dyffryn Conwy (Llanrwst) yn 1983. Mae leini fyny cynnar yn 
+cynnwys Emyr Davies yn canu, Dylan Hughes ar y drymiau, Mark Roberts ar y git&acirc;r a Barry 
+Cawley ar y bas.</p>
+
+<p>Yn gynnar, Mark, Dylan a Barry cyfaddef eu cariad am The Clash a'r caneuon maent yn 
+ysgrifennu, hyd yn oed pan canu yn y Gymraeg, roedd yn nodedig \"Clash\" sain. (Gwrandewch 
+ar y drumbeat ar ddechrau Tic Toc, yna gwrando ar y drumbeat ar ddechrau Radio Capital 
+One gan The Clash, a byddwch yn gweld yr hyn yr wyf yn ei olygu).</p>
+
+<p>Chwaraeodd Y Cyrff eu gig gyntaf yn Llansannan yn cefnogi Y Peth'ma ac wedi cwpwl o gigs, 
+y blynyddoedd cynnar leini fyny nid oedd diwethaf, Emyr symud i ffwrdd o'r ardal Llanrwst, 
+sy'n galluogi Mark i fynd i fyny ar lais fel y git&acirc;r. Mae'r band yn parhau i ymarfer eu set yn y 
+dyfodol. Roedd Barry a Mark gofynnodd Paul Jones i ddod i ymarfer. Cymerodd drosodd 
+chwarae bas oddi wrth Barry.</p>
+
+<p>Yn Ysgol Dyffryn Conwy, yr oedd eu hathro Daearyddiaeth, Toni Schiavone a oedd yn eu 
+hannog i ganu yn y Gymraeg. Yn ddiweddarach Toni oedd i gymryd cyfrifoldeb am archebu eu 
+gigs gyda Cymdeithas Yr Iaith Cymreig ac mae eu gig cyntaf.</p>
+
+<p>Y Cyrff gigged ar draws Gogledd Cymru. Yr oedd yn un o'r gigiau hyn yn 1984 eu bod yn 
+cwrdd Rhys Mwyn (bas o Yr Anhrefn) a gwahoddwyd hwy i recordio ddau drac ar gyfer y 
+casgliad \"Cam O'r Tywyllwch\" (rhyddhau ar cwmni record o Rhys 'Recordiau Anhrefn'). Y Cyrff 
+wedi recordio 'Tic Toc' a 'Lebanon' ar ei gyfer. Nid oedd yn hir cyn Y Cyrff rhyddhau dwy sengl 
+mwy - Pum Munud a Yr Haint. Erbyn y cam hwn mae'r band yn ymddangos yn rheolaidd ar 
+yr orsaf deledu S4C. Mae'r band yn wael am y explosure, hyd yn oed yn rheoli slot ar 'Stid'. 
+Nid oedd tan 1985/6 bod y cyfryngau Cymreig wedi dechrau i eistedd i fyny ac i gymryd sylw 
+o'r Cyrff.</p>
+
+<p>Mae'r geiriau y tu mewn Mae DDoe Yn DDoe:</p>
+
+<div class=\"quotes\">
+	&quot;Cymru, LLoegr a Llanrwst - a'r Y Cyrff! Y peth am Y Cyrff yw eu bod wedi 
+	cyrychioli yr hyn sydd yn arbennig am DDyffryn Conwy a LLanrst- rhyw fath o 
+	falchder ac annibyniaeth sydd yn nodweddiadol o'r lle a'r bobl. Roedd Y Cyrff yn 
+	cynrychoili Cymru'r wythdegau - Cyfres gigs \"LLanrwst yn LLosgi\" a'r holl 
+	grwpiau newydd a fu'n chwarae yno. Mae'r gerddoriaeth gorau erioed yn cyfleu 
+	syniad o amser ac ymdeimlad o le. Clywed Y Cyrff yn chwarae o flean eu pobl eu
+	hunain oedd clywed y gorau - mewn unrhyw iaith. Mwy na hynny, ar y llwyfan, Y
+	Cyrff oedd un o'r ychydig grwpiau oedd yn gorfodi eich sylw - perfformiadau 
+	hollol digyfawaddawd ac ysgytwol- nos Sul 'Steddfod LLanrwst neu nos Sadwrn 
+	Cwn Rhymni ac degau o nosweithia cyffelyb. Bydd swn Y Cyrff yn parhau i gyffroi,
+	bydd y caneuon yn sefydd gyda'r gorau y mae'r byd roc wedi eu cynhyrchu. Y grwp 
+	a oedd yn rhoi'r gerddoriaeth a'r perfformiadau ar y lyfan uwchben trifia'r byd 
+	roc, dyna'r neges i bod grwp arall. Ac roedd ganddynt rhywbeth i'w ddwend.&quot; 
+	<br />Tony
+</div>
+
+<p>Diolch Tony.</p>
+
+");
+define("PAGE_TWO_TEXT", "");
+
+?>
---
+++ includes/lang/en.about.php
@@ -0,0 +1,131 @@
+<?php
+$page1_link ="about.html";
+$page2_link ="about1.html";
+
+define("ENGLISH","English");
+define("WELSH","Welsh");
+define("PAGE_ONE","Page 1");
+define("PAGE_TWO","Page 2");
+
+define("PAGE_ONE_TEXT", '
+<style>
+.quotes{
+	font-style: italic;
+	margin: 0 auto;
+	width: 75%;
+}
+</style>
+<h1>Y Cyrff</h1>
+<p>Y Cyrff (The Bodies) was formed at Ysgol Dyffryn Conwy (LLanrwst School) in 1983. An early line-up included Emyr Davies on vocals, Dylan 
+Hughes on drums, Mark Roberts on guitar and Barry Cawley on bass. </p>
+<p>
+Early on, Mark, Dylan and Barry confessed their love of The Clash and the songs they composed, even when sung in Welsh, had a distinctive 
+&quot;Clash&quot; sound.(Listen to the drumbeat at the start of Tic Toc, then listen to the drumbeat at the start of Capital Radio One by 
+The Clash, and you will see what I mean). </p>
+<p>Y Cyrff played their first gig at LLansannan supporting Y Peth\'ma (These Things Here) and after a couple of gigs, The early line-up didn\'t last, 
+Emyr move away from the LLanrwst area, which enable Mark to take up on vocals as the guitar. The band continued to rehearse their future set. 
+Barry and Mark had asked Paul Jones to come to rehearsal. He took over playing bass from Barry.</p>
+<p>At Ysgol Dyffryn Conwy, it was their Geography teacher, Toni Schiavone who encouraged them to sing in Welsh. Later Toni was to take 
+responsibility for booking their gigs with Cymdeithas Yr Iaith Cymreig (Welsh Language Society) and their first gig. </p>
+<p>Y Cyrff gigged across North Wales. It was at one of these gigs in 1984 that they met Rhys Mwyn (bassist from Yr Anhrefn) who invited them to 
+record two tracks for the compilation "Cam O\'r Tywyllwch" (released on Rhys\' record label \'Recordiau Anhrefn\'). Y Cyrff had recorded \'Tic Toc\' and 
+\'Lebanon\' for it. It was not long before Y Cyrff released two more singles - Pum Munud and Yr Haint. By this stage the band were regularly appear 
+on the television station S4C. The band badly wanted the explosure, even managed a slot on 
+\'Stid\'. It was not until 1985/6 that the Welsh media started to sit up and take notice of 
+Y Cyrff.</p>
+<p>The text found inside Mae DDoe Yn DDoe:</p>
+<br>
+<div class="quotes">
+	&quot;Wales, England and LLanrwst and Y Cyrff! The thing about Y Cyrff is their 
+	being after respresenting speacially Conwy Valley and Llanrwst, some type of 
+	pride and independence is typically of the place and people. Y Cyrff was 
+	representing 80\'s Wales - series of gigs "LLanrwst yn LLosgi" (LLanrwst\'s 
+	burning) and all the new groups played there. Music is best ever conveying idea 
+	of time and feeling of the place. Hear Y Cyrff played in front of their people, 
+	themselves was hearing the best - in any language. More that, on stage, Y Cyrff 
+	was one of the little groups who was forcing your observation. Performances 
+	uncomprise and shaking - Sunday night Eisteddfod LLanrwst or Saturday Night 
+	Cwn Rhymni and tens of evenings, being the songs stands with the best that live 
+	rock produced. The group was giving music and the performances on stage that 
+	other grown live rock, there\'s a message to every other band. And was with 
+	something to say.&quot; - Toni
+</div>
+<p>Thank you Tony.</p>
+
+<div class="quotes">
+	&quot;Cymru, LLoegr a Llanrwst - a\'r Y Cyrff! Y peth am Y Cyrff yw eu bod wedi 
+	cyrychioli yr hyn sydd yn arbennig am DDyffryn Conwy a LLanrst- rhyw fath o 
+	falchder ac annibyniaeth sydd yn nodweddiadol o\'r lle a\'r bobl. Roedd Y Cyrff yn 
+	cynrychoili Cymru\'r wythdegau - Cyfres gigs "LLanrwst yn LLosgi" a\'r holl 
+	grwpiau newydd a fu\'n chwarae yno. Mae\'r gerddoriaeth gorau erioed yn cyfleu 
+	syniad o amser ac ymdeimlad o le. Clywed Y Cyrff yn chwarae o flean eu pobl eu
+	hunain oedd clywed y gorau - mewn unrhyw iaith. Mwy na hynny, ar y llwyfan, Y
+	Cyrff oedd un o\'r ychydig grwpiau oedd yn gorfodi eich sylw - perfformiadau 
+	hollol digyfawaddawd ac ysgytwol- nos Sul \'Steddfod LLanrwst neu nos Sadwrn 
+	Cwn Rhymni ac degau o nosweithia cyffelyb. Bydd swn Y Cyrff yn parhau i gyffroi,
+	bydd y caneuon yn sefydd gyda\'r gorau y mae\'r byd roc wedi eu cynhyrchu. Y grwp 
+	a oedd yn rhoi\'r gerddoriaeth a\'r perfformiadau ar y lyfan uwchben trifia\'r byd 
+	roc, dyna\'r neges i bod grwp arall. Ac roedd ganddynt rhywbeth i\'w ddwend.&quot; 
+	<br />Tony
+</div>
+
+<p>Diolch Tony.</p>');
+define("PAGE_TWO_TEXT", " <p>After appearing on T.V and the radio was starting to take 
+notice of the band, Y Cyrff were staring to gig over Wales. In 1986, the band played at National 
+Eisteddfod in RHyl. </p>
+
+<p>On the release front, Toni put out a few copies of Dan Y Cownter on cassette which 
+included studio and live tracks. The cassette sold out in a short time. </p>
+
+<p>In 1987, Sain released Y Testament Newydd. The band also appeared on The Grey Whistle 
+Test and The Tube.</p>
+
+<p>In 1988, Dylan left to join Yr Anhrefin. Mark Kendall replaced him. The band tourned 
+Poland. The Alarm had a habit of having a Welsh band supporting them and in November, 
+it was Y Cyrff's time to be the support act. They played Colwyn Bay to 1500 people. </p>
+
+<p>Another Eisteddfod  followed in the next year. This time the Eisteddfod was in LLanrwst. 
+A homecoming gig for the band! In the same year Yr Atgyfodi was released on vinyl on Probe 
+Plus. People at Rrobe Plus forget to put the title on. It contains the anthemn, Cymru, LLoegr a 
+LLanrwst. Anobaith o Awdl followed on the Ankst label. It contain live tracks 
+from Warsaw and Cardiff. Ankst was helping the band to get noticed outside of Wales. </p>
+
+<p>LLawenydd Heb DDiwedd was released on Ankst in 1991. LLawenydd Heb DDiwedd had 
+artwork by M.R. Swire and released on tape and vinyl. The inlay included the lyrics to Cwrdd, 
+Eithaf and Nunlle. LLawenydd Heb DDiwedd was critically acclaimed. </p>
+
+<p>The band released Mae DDoe Yn DDoe in 1992. The album was released on tape and C.D. 
+It is 22 track album which has songs as early as as Trwy'r Cymylau (1986) to Mewn Plu (1992). 
+Damwain Mewn FFatri Cyllell a FFyrc was a free tape with Mae DDoe Yn DDoe. Damwain Mewn
+FFatri Cyllell a FFyrc included Lebanon and Tic Toc from Cam O'r Tywyllwch, it also has songs 
+that appeared on T.V like Fy Enaid Noeth (Stid) and Hunanladdiad from Roc Rol Te, live BBC 
+sessions (Mewn Plu and Eithaf) and other live live version of songs (LLawenydd Heb DDiwedd 
+and Pethau Achlysurol).</p>
+
+<p>After the release of Mae DDoe Yn DDoe, Y Cyrff split up.</p>
+
+<p>LLwybr LLaethog did a re-mix of Y Cyrff's anthem, Cymru, LLoegr a LLanrwst, in 2000 in 
+memory of Barry C. The re-mix is called LLanrwst which feature a sample. taken from the 
+radio, of Mark Roberts taking about the writing of My Selfish Gene. </p>
+
+<p>Sherbet Antlers (Mark Roberts and Paul's music project with John Griffiths and Kevs Ford 
+of LLwybr LLaethog) did a \"cover\" of LLawenydd Heb DDiwed at a few of their gigs in 2004. 
+Since then a few artist (such as Elin FFlur, Alan Tan Lan, Cor y Cyrff) have done live or studio 
+covers of Y Cyrff tracks. </p>
+
+<p>Y Cyrff's history doesn't end here because Rasal (part of Sain) released the box set 
+Atalnod LLawn ion 2005. The box set had four C.Ds. C.D one had songs from Y Testament 
+Newydd on it, C.D two had songs from Yr Atgyfodi on it. C.D three had tracks from 
+LLawenydd Heb DDiwedd. C.D four contain the singles Pum Munud, Hwyl Fawr Heukwen / 
+Pethau Achylysurol, Yr Haint and live tracks. </p>
+
+<p>There was also a tour promoting the box set was called Cymru, LLoegr a LLanrwst with 
+gigs in London, Cardiff and LLanrwst. Y Cyrff didn't reform but  young Welsh artists 
+(Kentucky AFC, Alan Tan Lan, Maharashi, Dan Amor and Jen Jeniro) perform the gigs. </p>
+
+<p>So is this the final chapel of Y Cyrff's history? The band who are remembered by their 
+fans and press alike for their crafted songs, their passion and their talents and mysteries. 
+The band who gave us the anthem, Cymru, LLoegr a LLanrwst. The band who gave their 
+fans so many memories. I don't think so. </p>");
+
+?>
