6326599cdec93fa5aba535c7d63317cc0e4914e3
Christine <ycyrffgroupie@gmail.com>
2014-01-04 22:40:20 +0000

    change db connection to PDO and using global vars

--- profiles/fanprofile.php
+++ profiles/fanprofile.php
@@ -1,33 +1,4 @@
 <?php 
-if (file_exists('../includes/connection.php')){
-	require('../includes/connection.php');
-}else{
-	require("connection.php");
-}
-
-
-if (isset( $_GET['profileid']) && ($_GET['profileid'] != NULL && $_GET['profileid'] !='' )){
-	$profile_id = $_GET['profileid']; 
-	$sql = 'SELECT title FROM pages, fan_profile WHERE pages.filename = fan_profile.filename
-	AND profileid = '.$profile_id;
-
-	$query = mysql_query($sql) or die('The error is here');
-	if ($query ){
-		if ($titlequery = mysql_fetch_assoc($query)){
-		//;
-			$title = $titlequery["title"];
-		}else{
-			$title =  'Error:';
-		}
-	}else{
-		$title =  'Error:';
-	}
-}else{
-	$title =  'Error:';
-	$profile_id  = 500000;
-	//echo 'The else statement';
-}
-
 include '../includes/header.php'; ?>
 
 <body>
@@ -38,18 +9,13 @@ include '../includes/header.php'; ?>
 	</ul>
 </div>
 
-<h1><?=$title;?></h1>
+<h1><?php echo isset($fileinfo["title"])?$fileinfo["title"]:'Error'; ?></h1>
 
-<?=getprofile($profile_id);?>
-
-
+<?php echo getprofile($profile_id);?>
 
 <?php echo backButton($filename ); ?>
 
-
 </div>
-
-
 
 </body>
 
