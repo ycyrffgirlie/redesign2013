945efff216f07d109a58aea6328f4285f90a30f3
Christine <ycyrffgroupie@gmail.com>
2014-01-05 10:41:36 +0000

    displaying error messages

--- includes/connection.php
+++ includes/connection.php
@@ -1,4 +1,4 @@
-<?php ini_set('error_reporting', E_ALL);try {	$database = new PDO('mysql: host=localhost; dbname=ycyrf718_Copy_cyrff',	'ycyrf718_ycyrff', 'y1986Cyrff');}catch (DATABASEException $error) {
+<?php ini_set('error_reporting', E_ALL);global $database;try {	$database = new PDO('mysql: host=localhost; dbname=ycyrf718_Copy_cyrff',	'ycyrf718_ycyrff', 'y1986Cyrff');}catch (DATABASEException $error) {
 		echo '<p>There is an error with the db. '.$error->getMessage();	exit;
 	}
 ?>
