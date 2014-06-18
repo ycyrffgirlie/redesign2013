9b9de8c821a29f8e4d0c3fc500a003a23a2650ad
Christine <ycyrffgroupie@gmail.com>
2014-06-18 15:35:40 +0100

    included the settings for development server

--- includes/connection.php
+++ includes/connection.php
@@ -1,4 +1,4 @@
-<?php ini_set('error_reporting', E_ALL);global $database;try {	$database = new PDO('mysql: host=localhost; dbname=ycyrf718_Copy_cyrff',	'ycyrf718_ycyrff', 'y1986Cyrff');}catch (DATABASEException $error) {
+<?php ini_set('error_reporting', E_ALL);global $database;if ($_SERVER["DOCUMENT_ROOT"] == '/var/www/websites/redesign2013'){	$login = 'mysql: host=localhost; dbname=Copy_cyrff';	$user = 'root';	//'christine';	$password = '';	//'yCyrff';}else{	$login = 'mysql: host=localhost; dbname=ycyrf718_Copy_cyrff';	$user = 'ycyrf718_ycyrff';	$password = 'y1986Cyrff';}try {	$database = new PDO($login, $user, $password);}catch (DATABASEException $error) {
 		echo '<p>There is an error with the db. '.$error->getMessage();	exit;
 	}
 ?>
