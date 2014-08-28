68809a7b7402143cb50bfb8c3b72675c82b9641e
Christine <ycyrffgroupie@gmail.com>
2014-08-29 00:47:23 +0100

    Changed the main stylesheet version number

--- includes/header.php
+++ includes/header.php
@@ -83,7 +83,7 @@ if ($fileinfo){
 <!--end need to know section-->
 <meta name="rating" content="General" />
 <link rel="shortcut icon" href="/images/icon/cyrff.ico">
-<link rel="stylesheet" type="text/css" href="/css/style.css?v0.4" /><!--IE couldn't get the style the other way. Don't know why.-->
+<link rel="stylesheet" type="text/css" href="/css/style.css?v0.5" /><!--IE couldn't get the style the other way. Don't know why.-->
  <link rel="stylesheet" type="text/css" href="/css/menu.css?v0.1" />
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
  <?php
@@ -127,7 +127,7 @@ echo $fileinfo["htmlcode"];
 <!--end need to know section-->
 <meta name="rating" content="General" />
 <link rel="shortcut icon" href="/images/icon/cyrff.ico" />
-<link rel="stylesheet" type="text/css" href="/css/style.css?v0.4" /><!--IE couldn't get the style the other way. Don't know why.-->
+<link rel="stylesheet" type="text/css" href="/css/style.css?v0.5" /><!--IE couldn't get the style the other way. Don't know why.-->
 <link rel="stylesheet" type="text/css" href="/css/menu.css?v0.1" />
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
  <?php
