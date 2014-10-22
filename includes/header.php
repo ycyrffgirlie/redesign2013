520d6534d050c6e2b5a4f28fde30d59752b30366
Christine <ycyrffgroupie@gmail.com>
2014-10-22 22:54:46 +0100

    Added the header class to the site.

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,21 @@
 <?php
+/*@Author; Christine A. Black
+@Version:0.13
+@todo: 
+
+Version 0.13 - Added the header class to the site.
+Version 0.12 - Started a new footer class.
+Version 0.11 - Created a footer class.
+Version 0.10 - Changed the main stylesheet version number.
+Version 0.9 - Added the vistor class and changed the version of the main style sheet.
+Version 0.8 - Changed the version of the main style sheet.
+Version 0.7 - Updated jquery library. 
+Version 0.6 - Changed db connection to PDO and using global vars.
+Version 0.5 - Added a check to see if a session has been started.
+Version 0.4 - Fixed the mysql error when the fanproile is access without the param.
+Version 0.3 - Added jquery.
+Version 0.2 - So it works with the subdomain.
+Version 0.1 - Added footer to the site, */
 
 global $filename , $profile_id, $fileinfo ;
 if (file_exists('/includes/connection.php')){
@@ -11,12 +28,14 @@ require_once 'functions.php';
 /*the included files.*/
 include 'class/visitor.class.php';
 include 'class/footer.class.php';
+include 'class/header.class.php';
 
 /*Set a new visitor class and sets a new footer class.*/
 $visitor = new visitor;
 $footer = new footer;
+$header = new header;
 
-$filename = $visitor ->get_filename();
+$filename = $header  ->get_filename();
 
 
 if ($filename == '/profiles/fanprofile.php'){
