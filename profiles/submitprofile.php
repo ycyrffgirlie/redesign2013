50a34080b98e5c750216707251af11702b80f647
Christine <ycyrffgroupie@gmail.com>
2016-02-21 18:13:30 +0000

    Added fan profile class to site

--- profiles/submitprofile.php
+++ profiles/submitprofile.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION)){
 }
 
 if (isset($_POST["name"] )){
-if ($_POST["name"] !=NULL && $_POST["name"] != ' ' &&  $_POST["email"]  != NULL && $_POST["email"] != ' ' ){
+	if ($_POST["name"] !=NULL && $_POST["name"] != ' ' &&  $_POST["email"]  != NULL && $_POST["email"] != ' ' ){
 
 	$name = $_POST["name"];
 	$location = $_POST["location"];
@@ -19,14 +19,8 @@ if ($_POST["name"] !=NULL && $_POST["name"] != ' ' &&  $_POST["email"]  != NULL 
 	$favealbum = $_POST["favealbum"];
 	$comments = $_POST["comments"];
 	$_GET["v"] = '';
-	
-	/*echo '<p>Post data: ';
-	print_r($_POST);
-	echo '</p>
-	<p>Session data: '.$hideEmailAddress ;
-	echo '</p>';
-	die();*/
-}else{
+
+	}else{
 	$_SESSION["profileName"] = $_POST["name"];
 	$_SESSION["profileLocation"] = $_POST["location"];
 	$_SESSION["profilWebsite"] = $_POST["website"];
@@ -37,7 +31,7 @@ if ($_POST["name"] !=NULL && $_POST["name"] != ' ' &&  $_POST["email"]  != NULL 
 	$_SESSION["profileComments"] = $_POST["comments"];
 	header('Location:http://'.$_SERVER["HTTP_HOST"].'/profiles/submitprofile.php?v=fail', true, 302);
 	exit ();
-}
+	}
 }
 
 if (isset($_GET["v"])){
@@ -48,305 +42,36 @@ if (isset($_GET["v"])){
 }
 
 include '../includes/header.php'; ?>
+
+
 <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
 <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
 
-<script>
-$('document').ready(function(){
-	$('input[name="favesong"]').keypress(function(){
-		var textEnter = $(this).val();
-		
-		//alert(textEnter);
-		if (textEnter){
-		
-		}
-	});
-	
-	var songs = [
-		"Ansicrwydd",
-		"Y Cyrifoldeb",
-		"Defnyddia Fi",
-		"Y Pleser",
-		"Fy Enaid Noeth",
-		"Y Deffro"
-	];
-	
-	var albums = [
-		"Dan y Cownter",
-		"Y Testament Newydd",
-		"Yr Atgyfodi",
-		"Awdl o Anobaith",
-		"LLawenydd Heb DDiwedd",
-		"Mae DDoe Yn DDoe",
-		"Damwain Mewn FFatri Cyllyll a FFyrc",
-		"Atalnod LLawn"
-	];
-	
-	$('input[name="favesong"]').autocomplete({
-		//source: songs
-		source: "profiles.ajax.php",
-		minLemgth: 2
-	});
-	
-	$('input[name="favealbum"]').autocomplete({
-		source: albums
-	});
-});
-</script>
+<body>
 
-<body>"
+<script src="/includes/javascript/submitProfile.js"></script>
+
 <div class="container">
-<div id="menu">
-	<ul class="menu">
-		<?php include '../includes/menu.html';?>
-	</ul>
-</div>
+	<div id="menu">
+		<ul class="menu">
+			<?php include '../includes/menu.html';?>
+		</ul>
+	</div>
 
-<h1>Submit a Profile/<span lang="cy">Anfon proffeil</span></h1>
+	<h1>Submit a Profile/<span lang="cy">Anfon proffeil</span></h1>
 
 <?php
 	if (isset($_POST["name"]) &&  $_GET["v"] != 'fail'){
 	//if ($_POST["name"] !=NULL && $_POST["name"] != ' ' &&  $_POST["email"]  != NULL && $_POST["email"] != ' ' ){
 		
 
-		$to ='"Christine" <ycyrffgroupie@gmail.com>';
-		$subject ="Fan profile submitted";
-		$html = "<!DOCTYPE HTML PUBLIC\"-//W3C//DTD HTML 4.01 Transitional//EN\"\"http://www.w3.org/TR/html4/loose.dtd\">
-<html>
-<head></head>
-		
-<body style=\"background-image:url('http://www.ycyrffgroupie.co.uk/images/background5.jpg'); background-color: #800080; width:100%;\">
-		
-<table>
-		
-	<tr>
-		<td style=\"width:5%;\">
-		</td>
-		<td style=\"width:93%;\">
-		<table>
-			<tr>
-				<td style=\"width: 25%;\"><p style=\"color: #ffd700;\">Name/<span lang=\"cy\">Enw:</span>&nbsp;</p></td>
-				<td style=\"width: 75%;\"><p style=\"color: #ffd700;\">".$name."</p></td>
-			</tr>
-		<tr>
-			<td style=\"width: 25%;\"><p style=\"color: #ffd700;\">Location/<span lang=\"cy\">LLe:</span>&nbsp;</p></td>
-			<td style=\"width: 75%;\"><p style=\"color: #ffd700;\">".$location."</p></td>
-		</tr>
-		<tr>
-			<td style=\"width: 25%;\"><p style=\"color: #ffd700;\">Website/<span lang=\"cy\">Gwe safle:</span>&nbsp;</p></td>
-			<td style=\"width: 75%;\"><a style=\"color: #ffd700;\" href=\"".$website."\">".$website."</a></td>
-		</tr>
-		<tr>
-			<td style=\"width: 25%;\"><p style=\"color: #ffd700;\">E-mail/<span lang=\"cy\">E-bost:</span>&nbsp;</p></td>
-			<td style=\"width: 75%;\"><a style=\"color: #ffd700;\" href=\"mailto:".$email."\">".$email."</p></td>
-		</tr>
-		<tr>
-			<td align=\"center\" colspan=\"2\"><p style=\"color: #ffd700;\">Where did you first heard about Y Cyrff/<span lang=\"cy\"> LLe naethoch chi cyntaf wedi clywed am Y Cyrff?</span></p> <br><p style=\"color: #ffd700;\">".$heardcyrff."</p></td>
-		</tr>
-		<tr>
-			<td colspan=\"2\"><p style=\"color: #ffd700;\">Favourite Cyrff song/<span lang=\"cy\">Hoff g&acirc;n Cyrff:</span>&nbsp;</p></td>
-		</tr>
-		".($favesong != NULL ? $favesong != ' ' ? '<tr>
-			<td colspan="2"><p style="color: #ffd700;">'.$favesong.'</p></td>
-		</tr>' : ''  : '' )."
-		<tr>
-			<td colspan=\"2\"><p style=\"color: #ffd700;\">Favoutite Cyrff album/<span lang=\"cy\">Hoff albwn Cyrff:</span>&nbsp;</p></td>
-		</tr>
-		".($favealbum != NULL ? $favealbum != ' ' ?  '<tr>
-			<td colspan="2"><p style="color: #ffd700;">'.$favealbum.'</p></td>
-		</tr>' : '' : '' )."
-		<tr>
-			<td align=\"center\" colspan=\"2\"><p style=\"color: #ffd700;\">Other comments/<span lang=\"cy\"> Eraill sywl:</span></p> <br><p style=\"color: #ffd700;\">".$comments."</p></td>
-	</table>
-	</td>
-	</tr>
-</table>
-</body>
-</html>";
-
-		$txt = strip_tags(str_replace('&nbsp;',' ',$html));
-		
-		$semi_rand = md5(time()); 
-		$mime_boundary = "==Multipart_Boundary_x".$semi_rand ."x";
-		
-		$headers ='From: "Webmistress" <webmistress@ycyrffgroupie.co.uk>'."\r\n";
-		$headers .='Reply-To: "'.$name.'"<' .$email.'>'."\r\n";
-		$headers .='MIME-Version: 1.0'."\r\n";
-		$headers .= 'Content-Type: multipart/alternative; boundary='.$mime_boundary."\r\n";
-		//$headers .= 'Content-Type: text/html'."\r\n";
-		
-		$message = 'This is a multi-part message in MIME format.'."\r\n\r\n".
-		
-'--'.$mime_boundary."\r\n".
-'Content-Type: text/plain; charset="utf-8"'."\r\n".
-'Content-Transfer-Encoding:7-bit'."\r\n\r\n".$txt."\r\n".
-'--'.$mime_boundary."\r\n".
-'Content-Type: text/html; charset="utf-8"'. "\r\n".
-'Content-Transfer-Encoding: 7-bit'. "\r\n\r\n".$html."\r\n";
-
-		mail($to,$subject,$message, $headers);
-		
-		require('/home/ycyrf718/public_html/redesign2013/includes/connection.php');
-		
-		$sql= sprintf("INSERT INTO fan_profile ( name, place, website, email, email_hidden, firstheard, favesong, favealbum, comment) 
-			VALUES(%s,%s,%s,%s, ".$hideEmailAddress.",%s,%s,%s,%s)",
-			$database ->quote($name),
-			$database ->quote($location),
-			$database ->quote($website),
-			$database ->quote($email),
-			$database ->quote($heardcyrff),
-			$database ->quote($favesong),
-			$database ->quote($favealbum),
-			$database ->quote($comments));
-
-		$query = $database ->prepare($sql);
-		$query ->execute();
-	
-	//echo "<p>".$sql."</p>";
-	//die();
-		echo "<p>Thank you, your profile has been submitted. I will add your profile soon so come back to check.</p>
-	<p>Diolch i chi, eich proffil wedi ei gyflwyno. Byddaf yn ychwanegu eich proffil fuan er mwyn dod yn &ocirc;l i wirio.</p>";
+		echo $fanProfile ->submit_form_success($name,$location, $website, $email, $hideEmailAddress, $heardcyrff , $favesong, $favealbum, $comments);
 	
 	
 }else{
 	
-echo "<style>
+	echo $fanProfile ->display_submit_form();
 
-</style>
-
-<script>
-$('document').ready(function(){
-	$('div#error').hide();
-	
-	$('#profileForm').submit(function(){
-		var name = $('input[name=\"name\"]').val();
-		var email = $('input[name=\"email\"]').val();
-		var atPosition = email.indexOf('@');
-		var dotPosition= email.lastIndexOf('.'); 
-		var error = 0;
-		
-		if (name == 'null' || name.length == 0){
-			if (error == 0){
-				$('input[name=\"name\"]').focus();
-			}
-			error = error + 1;
-		}
-		
-		if (email == 'null' || email.length == 0){
-			if (error == 0){
-				$('input[name=\"email\"]').focus();
-			}
-			error = error + 1;
-		}
-		
-		if (atPosition <1 || dotPosition<atPosition+2 || dotPosition+2>=email.length){
-			if (error == 0){
-				$('input[name=\"email\"]').focus();
-			}
-			error = error + 1;
-		}
-		
-		if (error == 0){
-			return true;
-		}else{
-			$('div#error').show();
-			alert(\"Please check that you have enter your name and email address correctly. \\n Plis gwiriwch eich bod wedi rhoi eich enw a\'ch cyfeiriad e-bost yn gywir.\");
-			return false;
-		}
-		
-	})
-
-});
-</script>
-
-<form name=\"profileForm\" method=\"post\" action=\"submitprofile.php\" id=\"profileForm\">
-
-". (isset($_GET["v"] )?$_GET["v"] == 'fail'? '<p class="white">Please check that you have enter your name and email address correctly.
-	<br />
-	Plis gwiriwch eich bod wedi rhoi eich enw a\'ch cyfeiriad e-bost yn gywir.</p>'  :   '<div id="error">
-	<p class="white">Please check that you have enter your name and email address correctly.
-	<br />Plis gwiriwch eich bod wedi rhoi eich enw a\'ch cyfeiriad e-bost yn gywir.</p>
-</div>':'' )."
-
-<p class=\"white\">* Required information</p>
-<div id=\"content\">
-	<div class=\"fieldline\">
-		<div class=\"fieldname\"><p>Name/<span lang=\"cy\">Enw:</span><span class=\"white\">*</span>&nbsp;</p></div>
-		<div class=\"field\">
-			<p><input type=\"text\" size=\"20\" name=\"name\" ".(isset($_SESSION["profileName"])? 'value="'.$_SESSION["profileName"].'"' : '')."></p>
-		</div>
-	</div>
-	<div class=\"clear\">
-	</div>
-	<div class=\"fieldline\">
-		<div class=\"fieldname\"><p>Location/<span lang=\"cy\">LLe:</span>&nbsp;</p></div>
-		<div class=\"field\">
-			<p><input type=\"text\" size=\"20\" name=\"location\" ".(isset($_SESSION["profileLocation"]) ? 'value="'.$_SESSION["profileLocation"].'"' : '')."></p>
-		</div>
-	</div>
-	<div class=\"clear\">
-	</div>
-	<div class=\"fieldline\">
-		<div class=\"fieldname\"><p>Website/<span lang=\"cy\">Gwe safle:</span>&nbsp;</p></div>
-		<div class=\"field\">
-			<p><input type=\"text\" size=\"20\" name=\"website\" ".(isset($_SESSION["profilWebsite"])? 'value="'.$_SESSION["profilWebsite"].'"' : '')."></p>
-		</div>
-	</div>
-	<div class=\"clear\">
-	</div>
-	<div class=\"fieldline\">
-		<div class=\"fieldname\"><p>E-mail/<span lang=\"cy\">E-bost:</span><span class=\"white\">*</span>&nbsp;</p></div>
-		<div class=\"field\"><p><input type=\"text\" size=\"20\" name=\"email\" ".(isset($_SESSION["profileEmail"])? 'value="'.$_SESSION["profileEmail"].'"' : '' )."></p></div>
-	</div>
-	<div class=\"fieldline\">
-		<div class=\"fieldname\" style=\"width: 250px;padding-right: 5px;\">
-			<p>Do you wish for your email address to be hidden?</p>
-		</div>
-		<div class=\"field\" style=\"width: 60px;padding-left:75px;\">
-			<p>
-				<select name=\"hideEmailAddress\">
-					<option>Yes</option>
-					<option>No</option>
-				</select>
-			</p>
-		</div>
-	</div>
-	<div>
-		<div align=\"center\">
-			<p>Where did you first heard about Y Cyrff/<span lang=\"cy\"> LLe naethoch chi cyntaf wedi clywed am Y Cyrff?</span></p> 
-			<br><textarea cols=\"20\" rows=\"5\" name=\"heardcyrff\">".(isset($_SESSION["profileHeardcyrff"])? $_SESSION["profileHeardcyrff"] : '' )."</textarea>
-		</div>
-	</div>
-	<div>
-		<p>Favourite Cyrff song/<span lang=\"cy\">Hoff g&acirc;n Cyrff:</span>&nbsp;
-		<br />
-		<input type=\"text\" size=\"20\" name=\"favesong\" ".(isset($_SESSION["profileFavesong"])? 'value="'.$_SESSION["profileFavesong"].'"' : '')."></p>
-	</div>
-	<div class=\"clear\">
-	</div>
-	<div>
-		<p>Favoutite Cyrff album/<span lang=\"cy\">Hoff albwn Cyrff:</span>&nbsp;
-		<br />
-		<input type=\"text\" size=\"20\" name=\"favealbum\" ".(isset($_SESSION["profileFavealbum"] )? 'value="'.$_SESSION["profileFavealbum"].'"' : '')."></p>
-	</div>
-	<div>
-		<div align=\"center\">
-			<p>Other comments/<span lang=\"cy\"> Eraill sywl:</span></p>
-			<br><textarea cols=\"20\" rows=\"5\" name=\"comments\">".(isset($_SESSION["profileComments"])? $_SESSION["profileComments"] : '')."</textarea>
-		</div>
-	</div>
-
-	<br>
-	<br>
-
-	<div style=\"margin:0px auto; width:130px;\">
-		<input type=\"submit\" name=\"submit\" value=\"submit\"><input type=\"reset\" name=\"reset\" value=\"reset\">
-	</div>
-
-</div>
-</form>
-
-<div class=\"clear\"></div>" ;
 }
 ?>
 
