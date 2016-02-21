d0a179c92aad3dc90ef6b90181f8a06dd8b1f8d5
Christine <ycyrffgroupie@gmail.com>
2016-02-21 18:07:36 +0000

    Added fan profile class to site.

--- includes/functions.php
+++ includes/functions.php
@@ -54,51 +54,6 @@ $(document).ready(function(){
 	return $output;
 }
 
-function getprofile($profileid,$title){
-	if (file_exists('/includes/connection.php')){
-	require('includes/connection.php');
-}else{
-	require("connection.php");
-}
 
-	$sql = 'SELECT 
-			profileid, name, place, website, email, email_hidden, firstheard,favesong, favealbum, comment
-			FROM fan_profile 
-			WHERE profileid ='.$profileid;
-	
-	$query = $database ->prepare($sql);
-	$query ->execute();
-	
-	$profileinfo = $query ->fetch();
-	
-	if ($profileinfo){
-	//print_r($profileinfo);
-	
-	$output = '<h1> '.( isset($title)?$title:$profileinfo['name'].'\'s profile') .'</h1>
-			<p><strong>Name/Enw:</strong>&nbsp;'.$profileinfo['name'].'
-			<br>
-			<strong>Location/<span lang="cy">LLe:</span></strong>&nbsp;'.$profileinfo['place'].'
-			<br>
-			<strong>Website/<span lang="cy">Gwe safle:</span></strong>&nbsp;'.$profileinfo['website'].'
-			<br>
-			<strong>E-mail/<span lang="cy">E-bost:</span></strong>&nbsp;'.
-			($profileinfo["email_hidden"] == 0?$profileinfo['email']:'').'
-			<br>
-			<strong>Where first heard about Cyrff?/<span lang="cy">LLe naethoch chi cyntaf wedi clywed am Y Cyrff?</span></strong>&nbsp;'.$profileinfo['firstheard'].'
-			<br>
-			<strong>Favourite Cyrff song/<span lang="cy">Hoff g&acirc;n Cyrff:</span></strong>&nbsp;'.$profileinfo['favesong'].'
-			<br>
-			<strong>Favorite Cyrff album/<span lang="cy">Hoff albwn Cyrff:</span></strong>&nbsp;'.$profileinfo['favealbum'].'
-			<br>
-			<strong>Other comments/<span lang="cy">Eraill sywl:</span></strong>&nbsp;'.$profileinfo['comment'].'
-			</p>';
-		}else{
-			$output = '<h1> '.( isset($title)?$$title:'Error') .'</h1>
-			<p>This profile doesn\'t exist</p>';
-		}
-	
-	
-	return $output;
-}
 
 ?>
