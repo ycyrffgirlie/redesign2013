a79bced41c98d00abd9b641c986b5f1d3cab0f0d
Christine <ycyrffgroupie@gmail.com>
2017-04-19 00:32:02 +0100

    added a javascript for contact form

---
+++ includes/javascript/contact.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+	$("a.contact").click(function(event){
+		event.preventDefault();
+				
+		$('body').prepend('<div id="overlay"></div>');
+		$('.contactScreen').show();
+	});
+
+	$('#contactForm').submit(function(){
+		var name = $('input[name="name"]').val();
+		var email = $('input[name="email"]').val();
+		var atPosition = email.indexOf('@');
+		var dotPosition= email.lastIndexOf('.'); 
+		var error = 0;
+		
+		if (name == 'null' || name.length == 0){
+			if (error == 0){
+				$('input[name="name"]').focus();
+			}
+			error = error + 1;
+		}
+		
+		if (email == 'null' || email.length == 0){
+			if (error == 0){
+				$('input[name="email"]').focus();
+			}
+			error = error + 1;
+		}
+		
+		if (atPosition <1 || dotPosition<atPosition+2 || dotPosition+2>=email.length){
+			if (error == 0){
+				$('input[name="email"]').focus();
+			}
+			error = error + 1;
+		}
+		
+		if (error == 0){
+			return true;
+		}else{
+			$('div#error').show();
+			alert("Please check that you have enter your name and email address correctly. \\n Plis gwiriwch eich bod wedi rhoi eich enw a\'ch cyfeiriad e-bost yn gywir.");
+			return false;
+		}
+		
+	})
+})
