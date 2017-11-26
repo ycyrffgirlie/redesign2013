30d9b69583672a36d967abe7b9d90569a24509dd
Christine A. Black <ycyrffgroupie@gmail.com>
2017-11-26 00:10:52 +0000

    Make sure each section starts on it own line.

--- includes/class/menu.class.php
+++ includes/class/menu.class.php
@@ -6,7 +6,8 @@ class menu{
 	function build_band_section(){
 		
 		$output = "";
-		$output .= '					<li>Band / Band
+		$output .= '					
+					<li>Band / Band
 						<ul>
 							<li><a href="/about.html">About / <span lang="cy">Am</span></a></li>
 							<li><a href="/discography.html">Discography / <span lang="cy">Discograffi</span></a></li>
@@ -21,7 +22,8 @@ class menu{
 	function build_gallery_section(){
 		
 		$output = "";
-		$output .= '					<li>Gallery / Galeri
+		$output .= '					
+					<li>Gallery / Galeri
 						<ul>
 							<li><a href="/sound_clips/index.html">Sound Clips / <span lang="cy">Swn FFeiliau</span></a></li>
 							<li><a href="/Images/index.html">Pictures / <span lang="cy">LLuniau</span></a></li>
@@ -36,7 +38,8 @@ class menu{
 	function build_fan_section($class = "menu"){
 		
 		$output = "";
-		$output .= '					<li>Fans / FFanau
+		$output .= '					
+					<li>Fans / FFanau
 						<ul>
 							<li><a href="/games/index.html">Games / <span lang="cy">Gemau</span></a></li>
 							<li><a href="/profiles/index.html">Profiles/ <span lang="cy">Proffeil</span></a></li>
@@ -51,7 +54,8 @@ class menu{
 	function build_me_section(){
 		
 		$output = "";
-		$output .= '					<li>Me / Fi
+		$output .= '					
+					<li>Me / Fi
 						<ul>
 							<li><a href="/thankyou.html">Thank You / <span lang="cy">Diolch</span></a></li>
 							<li><a href="/about_me.html">About Me / <span lang="cy">Am Fi</span></a></li>
@@ -66,7 +70,8 @@ class menu{
 	function build_media_section(){
 		
 		$output = "";
-		$output .= '					<li>Media / Cyfryngau
+		$output .= '					
+					<li>Media / Cyfryngau
 						<ul>
 							<li><a href="/lyrics/index.html">Lyrics / <span lang="cy">Geiriau</span></a></li>
 							<li><a href="/reviews/index.html">Reviews / <span lang="cy">Adolygiadau</span></a></li>
@@ -83,7 +88,8 @@ class menu{
 	function build_other_section(){
 		
 		$output = "";
-		$output .= '					<li>Other / Arall
+		$output .= '					
+					<li>Other / Arall
 						<ul>
 							<li><a href="/forsale.html">For Sale / <span lang="cy">Ar Wreth</span></a></li>
 							<li><a href="/download.html">Downloads / <span lang="cy">Lawrlwythi</span></a></li>
@@ -99,7 +105,8 @@ class menu{
 	function build_web_section(){
 		
 		$output = "";
-		$output .= '					<li>Web / Gwe
+		$output .= '					
+					<li>Web / Gwe
 						<ul>
 							<li><a href="/links.html">Links / <span lang="cy">Cysyllitiadau</span></a></li>
 							<li><!--<a href="/newguestbook.html" rel="nofollow">--><a href="/guestbook.html" rel="nofollow">Guestbook / <span lang="cy">Gwestai LLyfr</span></a></li>
