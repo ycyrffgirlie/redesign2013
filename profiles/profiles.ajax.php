e0e60ad862eb3d33059281cff295efe3a3f85ac0
Christine A. Black <ycyrffgroupie@gmail.com>
2017-12-22 21:01:47 +0000

    Changed the dayabase connection.

--- profiles/profiles.ajax.php
+++ profiles/profiles.ajax.php
@@ -1,8 +1,11 @@
 <?php
-require('../includes/connection.php');
+require('../includes/class/database.class.php');
 //echo json_encode($_GET);
 
 $term = $_GET['term'];
+
+$db = new database;
+$database = $db ->connect();
 
 $sql = "SELECT * FROM songs WHERE title LIKE '%".$term ."%'";
 $query = $database ->prepare($sql);
