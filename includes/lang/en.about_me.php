28e800f01ca31b3550ce210d7197ceb807f48d03
Christine A. Black <ycyrffgroupie@gmail.com>
2018-03-06 21:11:10 +0000

    Added the about me pages

---
+++ includes/lang/en.about_me.php
@@ -0,0 +1,145 @@
+<?php
+$page1_link ="about_me.html";
+$page2_link ="update_me.html";
+
+define("ENGLISH","English");
+define("WELSH","Welsh");
+define("PAGE_ONE","Page 1");
+define("PAGE_TWO","Page 2");
+
+define("PAGE_ONE_TEXT", '
+<h1>Me</h1>
+<p>
+Oh, a page about me. Okay where do I start? Let start with the basics, my name is Christine. I live in
+England - before you ask, "Are you Welsh?" The answer, sadly, is I am not.
+</p>
+<p>
+I know what you are thinking, How did you hear about Y Cyrff and why are you running a website about 
+them? (Sometimes I do not know the answer to that one myself.)
+</p>
+<div style="    width: 240px; height: 350px; float: right; padding-left: 15px; padding-top: 5px; padding-bottom: 5px;">
+	<img src="images/Christine.JPG" alt="me">
+</div>
+<p>
+The answer to the first one is that I am a <strong>huge</strong> fan of
+Catatonia. I had heard that Mark and Paul from Catatonia were in Y Cyrff. (Plus I also heard that Markie was
+on vocals - cool huh!) I have found LLwybr LLaethog\'s remix of LLanrwst on the internet. I guess, out of curiosity, I downloaded Trwy\'r Cymylau from the internet. (I know it is
+wrong but without it - I wouldn\'t have got into Y Cyrff therefore this website wouldn\'t have existed.) I wanted
+to know a bit more about the band, so I went on the internet to search for Y Cyrff links and found only one.
+The one I found was <a class="normal" target="_blank" href="http://www.llygredd.moesol.btinternet.co.uk/Cyrff.htm">cudd</a> which only had sound clips. In the meantime, I re-read the section on Y Cyrff
+in &quot;Cerys, Catatonia and the Rise of Welsh Pop&quot; by David Owens - it had very little on Y Cyrff. I also re-read
+&quot;To Hell and Back with Catatonia&quot; by Brian Wright - that had less about Y Cyrff then David Owens had in his
+book.
+<p>
+</p>
+After a year, I decided it was about time to buy a Y Cyrff album so I went on <a class="normal" target="_blank" href="http://www.ankst.net">Ankst\'s Site</a> and bought Mae
+DDoe Yn DDoe. (I have lost count how many times I have played that tape - I am sure if I play it
+one more time that my family will go mad. I think they just did!)
+<p>
+</p>
+Anyway going back to the story of how I became a Y Cyrff fan, I posted a message
+in a yahoo group asking if anyone knew anything about Y Cyrff. A couple messages later, I found out
+a Y Cyrff link is rare. I suggested that I could do a Y Cyrff website if I know a little bit more about
+Y Cyrff. Somebody advised me that I should do it anyway and maybe people would start telling me about
+the band. With no money in pocket (I am a student) and being a Welsh learner, I decided to start up a Y
+Cyrff website as an offshoot of my other site -  <a class="normal" target="_blank" href="http://uk.geocities.com/international_velvet2000">The Lads From Catatonia</a> which was what I was designing
+at that time. In May 2003, this website was born. Hosted by Geocities, it has four pages - home, about,
+contact me and a guestbook. The about page only had a paragraph on Y Cyrff.
+</p>
+<p>
+About a month later, I managed to find Cam O\'r Tywyllwch (which includes Tic Toc and Lebanon) on eBay.
+I placed my bid and won. I also managed to bid for Pum Mumud on eBay and again I won it.  During this
+period, I was e-mailing anyone that I thought might know something. This proved a little bit wasted - but I
+tried my best. I also added things like forum, vote page, chat room - all hosted by bravenet to my site.
+</p>
+
+<p>
+Maybe a month later, I was finding that my Y Cyrff collection was not quite big enough so I added
+LLawenydd Heb DDiwedd to it. This had the added advantage that it has lyrics to three songs - Cwrdd,
+Nunlle and Eithaf. So I added a lyrics page to the website.
+</p>
+<p>
+I sent another round of e-mails to my contacts. I got some pictures from somebody. I added a picture page to
+my website. I also added another counter because I could not believe that anyone was viewing the website.
+(At time of writing, I have something like 70 visitors to the website. At the moment I am waiting for another
+Y Cyrff vinyl. I don\'t think I have asked the question - "Is there any post for me?" so much in my life. I am also
+up and waiting beside my post box for it.) 
+</p>
+<p>
+I guess the reason why this website has had more visitors than my <a class="normal" target="_blank" href="http://uk.geocities.com/international_velvet2000">The Lads From Catatonia</a> website is
+because the world has lots of Catatonia links. Where as Y Cyrff have only a handful websites on them.
+</p>
+<p>
+The reason why am I doing this site
+<br />
+<ul>
+<li>To find out more about Y Cyrff</li>
+<li>To pass on information on Y Cyrff to others</li>
+<li>Help bring in new fans</li>
+</ul>
+<br />
+Now ask me if I prefer Y Cyrff or Catatonia. The trouble is I have no answer to this as comparing Mark
+Roberts\' guitar playing and lyrics with Mark Roberts\' guitar playing and lyrics or Paul Jones\' bass playing with
+Paul Jones\' playing , is hard as they are the same people. Also half of Y Cyrff became Catatonia. News has it
+Mark Roberts and Paul Jones have started a new group with John Griffiths and Kevs Ford of LLwybr
+LLaethog. (Sadly, while I was reading To Hell and Back with Catatonia, I found out that Barry C has died.
+My thoughts go out to Barry\'s friends and family on this sad news.)
+</p>
+<p>
+The funniest moment whilst running the website was when I was e-mailed some pictures of Y Cyrff. I had to
+do a double take when I saw Paul Jones with long hair. I even looked at the Catatonia poster that is hanging
+on my wall beside my computer. I could not believe that Paul could have long hair because he was the member
+of Catatonia that would not get drunk. I guess I only know Catatonia later on in their careers.
+</p>
+<p>
+I guess there will be a few more surprises in store for me. I hope I can continue this website for as long as
+possible. If it is not forever - I do not know when I will give this website up. 
+</p>
+<p>
+You can read my online <a class="normal" target="_blank" href="http://www.livejournal.com/users/y_cyrff_girlie/">journal</a> (It is kept in English).
+</p>');
+define("PAGE_TWO_TEXT", '<h1>Update on me</h1>
+
+Okay, an update on me and my Cyrff collection. Since writing the me page, back in November, I have received the Cyrff vinyl that I was waiting for. It was the 12&quot; untitled vinyl 
+with <em>Cymru, LLoegr a LLanrwst</em> on it. Also since writing this page Sherbet Antlers have released a single called <em>Hikikomorai/Let Yourself Go</em>, which I happen to have a copy 
+of in my collection of CDs - don\'t ask how many copies I have of it! La la la la la mmm la la la la ba ba la.
+<p>
+</p>
+I had an e-mail from someone asking if I wanted any pictures of the band. I said I would like pictures of Cyrff and they e-mailed me the pictures. Has anyone else got any other pictures of 
+the band? If they do, please, please e-mail them to me at <a href="mailto:y_cyrff_girlie@[nospam]yahoo.co.uk?subject=pictures/lluniau">y_cyrff_girlie@yahoo.co.uk</a>.
+<p>
+</p>
+Anyway going back to February, I went and saw Sherbet Antlers at the Clwb Ifor Bach and they did <em>LLawenydd Heb DDiwedd</em>. Afterwards I sent them an e-mail asking them for the 
+lyrics to <em>LLawenydd Heb DDiwedd</em>. They sent me the lyrics - oh boy they didn\'t know that I own the Cyrff site and that I was going to put them up on the site until I e-mailed 
+them asking for permission to use them on the site. Err I think I am in the doghouse!
+<p>
+</p>
+In March, I bought <em>Damwain Mewn FFatri Cyllyll a FFyrc</em> from eBay, which had the added advantage of a picture of the band on the cover - another picture of the band!
+<p>
+</p>
+Couple of months later, I managed to buy <em>Mae DDoe Yn DDoe</em> on tape, <em>LLawenydd Heb DDiwedd</em> on tape and <em>Damwain Mewn FFatri Cyllyll a FFyrc</em> from eBay. Now 
+I can have one set with me and another lot at home.
+<p>
+</p>
+I have got a digital camera so now I can take pictures of various Y Cyrff vinyl covers.
+<p>
+</p>
+I have also managed to buy <em>Mae DDoe Yn DDoe</em> on CD from eBay which will allow me to get sound clips from it instead of having to download the songs from the Internet. A very nice 
+fan copied a bootleg for me. I would like to thank that fan and so far I haven\'t stopped playing it - another thing to drive my family and friends up the wall with! Mmm they love that - 
+lots. ;-) Some day I will get my family to appreciate good music.
+<p>
+</p>
+I went on holiday to Wales. I went armed with my Cyrff folder and some thing to do, relating to the site. I asked about Y Cyrff but no luck. I finished writing the first draft of this page and 
+included this: &quot;I am currently in my hideout in Wales. I am drinking a bottle of beer so iechyd da o Gymru&quot;. 
+<p>
+</p>
+I am back from my holidays and listening to Y Cyrff.  I got some videos (two people have sent me these videos) to add to the site but I need to get permission to use them. Well there\'s a 
+re-design of the site to work on. It has become a routine for me, on most nights, to watch the videos from 10.30 p.m to 11.30. This has led family members to poke their heads around the 
+door and ask, &quot;You are not watching Cyrff again, are you?&quot; with a disappointed look while I am quickly trying to change the channel. I am sure that certain family members 
+are starting to worry about my sanity.  Oh well!
+<p>
+</p>
+I said I am currently taking from the running the site which I am. I have got lots of plans to meet up with friends that I haven\'t seen for a long time (unless you count down the pub).  I 
+know bad bad web mistress! I also got a room to tidy. I wonder what I will get for Christmas - a Cyrff record or two?
+</p>');
+?>
